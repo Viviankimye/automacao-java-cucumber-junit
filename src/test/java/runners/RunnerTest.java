f48acadc704f4003de7eb3c59e20579756cce611
Vivian <vkimye@gmail.com>
2026-03-03 23:06:04 -0300

    atualizado3

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -15,7 +15,7 @@ import io.cucumber.junit.CucumberOptions;
         //O plugin pretty deixa o output no console mais bonito e legível.
         plugin = {"pretty", "html:target/report-html"},
         //Remove caracteres especiais e cores do console.
-        monochrome = true,
+        monochrome = false,
         //Indica qual estilo o Cucumber deve usar ao gerar métodos automaticamente.
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         publish = false,
@@ -23,6 +23,6 @@ import io.cucumber.junit.CucumberOptions;
         //Verifica steps (true) ou executa testes (false)
         dryRun = false
 )
-public class Runner {
+public class RunnerTest {
 
 }
